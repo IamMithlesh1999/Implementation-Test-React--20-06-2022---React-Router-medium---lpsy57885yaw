adbf1c50d87538d9d602b0064006bc1d32f6319f
Mithlesh Kumar <100525662+IamMithlesh1999@users.noreply.github.com>
2022-06-20 19:51:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,14 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
-import { AppRoutes } from '../AppRoutes';
-import '../styles/App.css';
-import { NavBar } from './NavBar';
-const App = () => {
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "../AppRoutes";
 
+import "../styles/App.css";
+const App = () => {
   return (
     <BrowserRouter>
-      <div id="main">
-        <NavBar />
-        <AppRoutes />
-      </div>
+      <AppRoutes />
     </BrowserRouter>
-
-  )
-}
-
+  );
+};
 
 export default App;
